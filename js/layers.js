0eaffba5c3af2c9153734c37317a17caf36ed359
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-09 16:23:19 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -46,7 +46,7 @@ addLayer("p", {
             description: "Increase MJ gain based on MJs.",
             cost: new Decimal(15),
             effect(){
-                return player.points.add(1).pow(0.20)
+                return player.points.add(1).pow(0.075)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
         },
