27b2de829d4dc353f5ba128d4ec4e027141b76cb
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-09 14:49:42 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,6 +24,5 @@ addLayer("MJ", {
     hotkeys: [
         {key: "m", description: "M: Reset for MJ points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true},
-})
+    layerShown(){return true}
 	 
