ef78701bcba4d961e9293f242617d55968070183
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-10 18:29:08 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -87,5 +87,33 @@ addLayer("p", {
             done() { return player.p.points >= (1000000) }
         },
     },
-})
+}),
 
+addLayer("q", {
+    name: "Mega MJ Points", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "MMJ", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#c28d23",
+    requires: new Decimal(e22), // Can be a function that takes requirement increases into account
+    resource: "Mega MJ Points", // Name of prestige currency
+    baseResource: "MJs", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "m", description: "M: Reset for MJ points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+})
