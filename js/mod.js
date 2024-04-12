349fa574fc60a4de1488e830af693570be65020a
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-12 08:18:20 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -67,7 +67,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e70"))
+	return player.points.gte(new Decimal("e100"))
 }
 
 
