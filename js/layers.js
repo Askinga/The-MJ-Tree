d124d68932c57b25dfe0387381f88c44d8684979
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-13 10:36:14 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -243,7 +243,7 @@ addLayer("C", {
     baseResource: "Super MJ Points", // Name of resource prestige is based on
     baseAmount() {return player.S.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 1.7, // Prestige currency exponent
+    exponent: 2.15, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
 	return mult
