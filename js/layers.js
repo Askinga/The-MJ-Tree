b9d00542432bfaad615f87241097f9b705fba7ae
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-13 12:56:20 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -356,16 +356,6 @@ addLayer("G", {
             rewardDescription: "^1.05 Super MJ Points"
 	},
     },
-    buyables: {
-    11: {
-        cost(x) { return new Decimal(1).mul(x) },
-        display() { return "buy" },
-        canAfford() { return player[this.layer].points.gte(this.cost()) },
-        buy() {
-            player[this.layer].points = player[this.layer].points.sub(this.cost())
-            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
-        },
-    },
 })
 
 addLayer("H", {
