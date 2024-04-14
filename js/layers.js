56df077db5a082510f90b4eabe8d3545f61b583f
Askinga <152022166+Askinga@users.noreply.github.com>
2024-04-14 08:57:13 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -473,7 +473,7 @@ addLayer("L", {
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.08, // Prestige currency exponent
     passiveGeneration() {
-        if (hasUpgrade('L', 55)) return 100
+        if (hasUpgrade('L', 54)) return 100
 	if (hasUpgrade('L', 23)) return 3
 	return 0
     },
