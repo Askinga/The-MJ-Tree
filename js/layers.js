dbdc9e441ab88c435c6ed2adc59d8378458029b5
Askinga <152022166+Askinga@users.noreply.github.com>
2024-05-18 20:59:26 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1811,6 +1811,7 @@ addLayer("Bo", {
     },
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+	if (hasUpgrade('UT', 21)) mult = mult.times(2)
 	return mult
     },
 
