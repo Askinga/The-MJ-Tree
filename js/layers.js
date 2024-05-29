8480be13c32fe64ffabf50880784132bbc5cf3a4
Askinga <152022166+Askinga@users.noreply.github.com>
2024-05-29 19:56:02 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -213,3 +213,60 @@ addLayer("p", {
         },
     },
 })
+addLayer("up", {
+    name: "Upgraded Prestige Points",
+    symbol: "U",
+    position: 0,
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    nodeStyle: {
+	"border-radius": "100%",
+	"width": "80px",
+	"height": "100px"
+    },
+    color: "#0ec466",
+    requires: new Decimal(30000000), // Can be a function that takes requirement increases into account
+    resource: "upgraded prestige points", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+	return mult
+    },
+
+
+
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "u", description: "U: Reset for upgraded prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+
+    layerShown(){
+        let visible = false
+        if (hasUpgrade('p', 35) || player.up.unlocked) visible = true
+       return visible
+    },
+    branches:["p"],
+    tabFormat: {
+        "Main tab": {
+            content: [
+                "main-display",
+                "resource-display",
+                "prestige-button",
+                "blank",
+                "blank",
+                "blank",
+                "blank",
+                "blank",
+                "blank",
+                "upgrades"
+            ],
+        },
+    },
