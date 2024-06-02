6cd02fa032a9757c2212bc0d998b8e4220e74050
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-02 11:24:15 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("p", {
+ addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -329,24 +329,44 @@ addLayer("up", {
 })
 
 addLayer("au", {
-    name: "automation",
-    symbol: "Au", // ↓
+    name: "Automation",
+    symbol: "Au",
     position: 3,
-    startData() {return {
+    startData() { return {
         unlocked: true,
-        points: new Decimal (0),
+		points: new Decimal(0),
     }},
-    tooltip() {
-        return "Automation"
+    nodeStyle: {
+	"border-radius": "100%",
+	"width": "110px",
+	"height": "100px"
     },
     color: "#800000",
-    requires: new Decimal(10000000),
+    requires() {
+        let req = new Decimal(10000000)
+        return req
+    }, // Can be a function that takes requirement increases into account
     resource: "automation points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points},
-    type: "static",
-    exponent: 1.5,
-    row: "side",
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+	return mult
+    },
+
+
+
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: "side", // Row the layer is in on the tree (0 is the first row)
+
+    layerShown(){
+        let visible = true
+       return visible
+    },
     tabFormat: {
         Difficulty:{
             unlocked() {return false},
@@ -358,6 +378,6 @@ addLayer("au", {
                 return "Next automation point at "+format(getPointDivider())+" points!"
             }],
         ],
-        }
+        },
     },
 })
