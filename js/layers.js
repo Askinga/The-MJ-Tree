8d2f470be47ea2536f54f9f6b2c14e204d090955
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-02 11:27:09 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
- addLayer("p", {
+addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -336,11 +336,6 @@ addLayer("au", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    nodeStyle: {
-	"border-radius": "100%",
-	"width": "110px",
-	"height": "100px"
-    },
     color: "#800000",
     requires() {
         let req = new Decimal(10000000)
