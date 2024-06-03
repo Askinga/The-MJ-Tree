322ad9f789f6b35615901d00443cbf07e6a4d4df
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-03 07:39:37 -0400

    Delete js/particles.js

--- js/particles.js
+++
@@ -1,9 +0,0 @@
-const myParticle {
-    image:"https://galaxy.click/pfp/medium/6431-1717368048961.webp",
-    spread: 20,
-    gravity: 2,
-    time: 3,
-    speed() { // Randomize speed a bit
-        return (Math.random() + 1.2) * 8 
-    },
-}
