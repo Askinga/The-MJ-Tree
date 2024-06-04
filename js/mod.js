a2e63fd9e7293b8b3c4e082b97833fe2bdb1a939
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-04 15:21:45 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -68,8 +68,6 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
-        () => (player.points.gte(1e9)&&(canGenPoints())) ? "Points ^0.75 because of softcap" : "",
-	"<br>",
 ]
 
 // Determines when the game "ends"
