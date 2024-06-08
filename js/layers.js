ad808159471ee19ffd984dc45fca3c0fdde81962
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-07 22:53:56 -0400

    Fix bug where upgrade 25 was not showing

--- js/layers.js
+++ js/layers.js
@@ -2171,7 +2171,7 @@ addLayer("UT", {
                 "main-display",
                 "prestige-button",
                 "blank",
-				["upgrade-tree", [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32]]]
+				["upgrade-tree", [[11, 12, 13, 14], [21, 22, 23, 24, 25], [31, 32]]]
             ]
         },
     },
