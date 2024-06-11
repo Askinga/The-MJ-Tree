3a541d4f6c17e331386b13ad8c51ec017a63d6be
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-11 18:42:08 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,35 +1,34 @@
 let modInfo = {
-	name: "The MJ Tree",
-	id: "abc123",
+	name: "The MJ Tree REWRITTEN",
+	id: "mymod1",
 	author: "Askinga",
-	pointsName: "MJs",
+	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
-	initialStartPoints: new Decimal (0), // Used for hard resets and new players
+	initialStartPoints: new Decimal (10), // Used for hard resets and new players
 	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "1.6.0 Very big update",
-	name: "A very big update",
+	num: "0.0",
+	name: "Literally nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v1.6.0 Very big update</h3><br>
-		- More updates soon! Endgame e23010 MJs.<br>
-		- Added more upgrades and 4 more achievements! Added a savebank just in case you lose your progress. Added a second upgrade tree. Added another softcap. Added a extension to MJ Point upgrades. Added a MJ Booster layer cap. and more<br>
-                - Check back here for more updates! Full changelog at <a href=https://galaxy.click/updates/355>galaxy.click/updates/355</a>`
-  
+	<h3>v0.0</h3><br>
+		- Added things.<br>
+		- Added stuff.`
+
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
 var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
 
-function getStartPoints(){  
+function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
 }
 
@@ -43,36 +42,7 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
-	if (layers.Bo.effect().gte(1)) gain = gain.times(layers.Bo.effect())
-        gain = gain.times(buyableEffect('Po', 11))
-	if (hasUpgrade('p', 11)) gain = gain.times(2)
-	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
-	if (hasUpgrade('p', 13)) gain = gain.times(upgradeEffect('p', 13))
-	if (hasUpgrade('p', 21)) gain = gain.times(10)
-	if (hasUpgrade('p', 22)) gain = gain.times(upgradeEffect('p', 22))
-	if (hasUpgrade('p', 24)) gain = gain.times(1000)
-	if (hasUpgrade('S', 11)) gain = gain.times(10)
-	if (hasUpgrade('S', 14)) gain = gain.pow(1.05)
-	if (hasUpgrade('G', 11)) gain = gain.times(1e6)
-	if (hasUpgrade('G', 12)) gain = gain.times(upgradeEffect('G', 12))
-	if (hasUpgrade('H', 11)) gain = gain.times(1e50)
-	if (hasUpgrade('H', 12)) gain = gain.times(1e20)
-	if (hasUpgrade('L', 41)) gain = gain.pow(1.01)
-	if (hasMilestone('p', 0)) gain = gain.times(4)
-	if (hasMilestone('p', 1)) gain = gain.times(15)
-	if (hasMilestone('p', 2)) gain = gain.times(250)
-        if (hasUpgrade('b', 11)) gain = gain.times(upgradeEffect('b', 11))
-	if (hasAchievement('a', 21)) gain = gain.times(1.5)
-        if (hasUpgrade('Po', 11)) gain = gain.times(100)
-	if (hasUpgrade('UT', 11)) gain = gain.times(1e10)
-	if (hasUpgrade('UT', 22)) gain = gain.pow(1.01)
-	if (hasUpgrade('UT', 31)) gain = gain.times(1e100)
-	if (hasUpgrade('UT', 13)) gain = gain.times(1e200)
-	if (hasUpgrade('UT', 24)) gain = gain.pow(1.02)
-	if (hasUpgrade('p', 31)) gain = gain.times(1e50)
-	if (hasUpgrade('UT', 26)) gain = gain.times(1e150)
-	if (hasUpgrade('UT', 33)) gain = gain.times(1e300)
+	let gain = new Decimal(0)
 	return gain
 }
 
@@ -82,12 +52,11 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
-    "Get e23010 MJs to beat the game!"  
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return (player.points.gte("e23010"))
+	return player.points.gte(new Decimal("e280000000"))
 }
 
 
