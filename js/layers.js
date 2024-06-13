203b1afb57ad0109a9aa279f8c316aa5ddefcd8b
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-12 20:03:02 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -45,7 +45,7 @@ addLayer("p", {
             description: "Add to base point gain based on points.",
             cost: new Decimal(20),
             effect(){
-                return player.points.add(1).pow(0.05)
+                return player.points.add(1).pow(0.05).times(10)
             },
             effectDisplay() { return "+" + format(upgradeEffect(this.layer, this.id)) }, // Add formatting to the effect
             unlocked() { return (hasUpgrade('p', 11)) },
