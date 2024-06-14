f5770d94e122541e808e21d61753ba94a96210bf
tmt-master <maah6928@rscloud.ca>
2024-06-14 12:09:47 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -28,7 +28,7 @@ addLayer("p", {
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    resetsNothing: if (hasMilestone('p', 0,)),
+    resetsNothing: if (hasMilestone('p', 0,)) return true,
     hotkeys: [
         {key: "m", description: "M: Reset for mastered points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
