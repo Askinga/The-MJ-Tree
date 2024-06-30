d130331b51b70ba7d9ee2cfebc9bac39c53a6304
Askinga <152022166+Askinga@users.noreply.github.com>
2024-06-30 16:40:55 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -84,6 +84,7 @@ function getPointGen() {
 	if (hasMilestone('m', 0)) gain = gain.times(100)
 	if (hasMilestone('m', 6)) gain = gain.times(1e10)
 	if (hasMilestone('m', 11)) gain = gain.times(1e20)
+	if (hasAchievement('🏆', 25)) gain = gain.times(1e15)
 	
 	// power
 	
