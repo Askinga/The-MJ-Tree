2abadb681d003064acbb94b74c4f51f775c41f4b
Askinga <152022166+Askinga@users.noreply.github.com>
2024-07-05 15:57:08 -0400

    Update themes.js

--- js/utils/themes.js
+++ js/utils/themes.js
@@ -23,7 +23,7 @@ var colors = {
 		background_tooltip: "rgba(0, 15, 31, 0.75)",
 	},
         good: {
-		1: "#091f1a",//Branch color 1
+		1: "#ffffff",//Branch color 1
 		2: "#bfbfbf",//Branch color 2
 		3: "#7f7f7f",//Branch color 3
 		color: "#022920",
