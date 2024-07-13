026b3e20dfa361797d407f9b87a3b817d1dd95d1
Askinga <152022166+Askinga@users.noreply.github.com>
2024-07-13 17:27:22 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -44,7 +44,7 @@ function getPointGen() {
 
 	let gain = new Decimal(1)
 	if (hasUpgrade('p', 11)) gain = gain.times(2)
-	if (hasUpgrade('p', 112)) gain = gain.times(3)
+	if (hasUpgrade('p', 12)) gain = gain.times(3)
         if (hasUpgrade('p', 21)) gain = gain.times(upgradeEffect('p', 21))
 	if (hasUpgrade('p', 22)) gain = gain.times(upgradeEffect('p', 22))
 	return gain
