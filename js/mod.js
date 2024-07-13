aa40ae8154c864e976d239c72d3df361b80a22db
Askinga <152022166+Askinga@users.noreply.github.com>
2024-07-12 21:25:19 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Upgrade Tree TMT",
+	id: "mymod2",
+	author: "Askinga",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
