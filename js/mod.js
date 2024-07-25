e8102d2af01e893fb7d3b18c152a39e8f76ed3e9
Askinga <152022166+Askinga@users.noreply.github.com>
2024-07-25 14:46:58 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,13 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "2.0.0 Early release",
-	name: "A very big update",
+	num: "2.0.0!",
+	name: "yay!",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v1.6.0 Very big update</h3><br>
-		- More updates soon! Endgame e23010 MJs.<br>
+	<h3>v2.0.0!</h3><br>
+		- More updates soon! Endgame e2130000 MJs.<br>
 		- Added more upgrades and 4 more achievements! Added a savebank just in case you lose your progress. Added a second upgrade tree. Added another softcap. Added a extension to MJ Point upgrades. Added a MJ Booster layer cap. and more<br>
                 - Check back here for more updates! Full changelog at <a href=https://galaxy.click/updates/355>galaxy.click/updates/355</a>`
   
@@ -91,12 +91,12 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
-    "Get e23010 MJs to beat the game!"  
+    "Get e2130000 MJs to beat the game!"  
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return (player.points.gte("e23010"))
+	return (player.points.gte("e2130000"))
 }
 
 
