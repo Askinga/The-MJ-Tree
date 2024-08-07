64c9a6b50c3df166b906a00b055143a4c437ec4d
Askinga <152022166+Askinga@users.noreply.github.com>
2024-08-07 19:07:23 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -10,7 +10,7 @@ addLayer("p", {
     }},
     color: "#ffffff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "Clicks", // Name of prestige currency
+    resource: "clicks", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
