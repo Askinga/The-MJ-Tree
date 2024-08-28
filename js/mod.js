a57dcbcf2c44ca3d46b504c6ef493e65b6317271
Askinga <152022166+Askinga@users.noreply.github.com>
2024-08-28 17:17:09 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -72,6 +72,7 @@ function getPointGen() {
 	if (hasChallenge('up', 11)) gain = gain.times(10)
 	if (inChallenge('up', 11)) gain = gain.pow(0.5)
 	if (hasUpgrade('up', 24)) gain = gain.times(100)
+	if (hasUpgrade('up', 25)) gain = gain.times(upgradeEffect('up', 25))
 	return gain
 }
 
