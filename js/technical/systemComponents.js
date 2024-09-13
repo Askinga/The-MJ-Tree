60b78ea6faac8716683b425b1ed6f94cd39f60a9
Askinga <152022166+Askinga@users.noreply.github.com>
2024-09-13 18:50:10 -0400

    Update systemComponents.js

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -171,7 +171,9 @@ var systemComponents = {
                 <td><button class="opt" onclick="toggleOpt('forceOneTab'); needsCanvasUpdate = true">Single-Tab Mode: {{ options.forceOneTab?"ALWAYS":"AUTO" }}</button></td>
 				<td><button class="opt" onclick="toggleOpt('forceTooltips'); needsCanvasUpdate = true">Shift-Click to Toggle Tooltips: {{ options.forceTooltips?"ON":"OFF" }}</button></td>
 				</tr> 
-        </table>`
+       	     <tr>
+	        <td><button class="opt" onclick="toggleOpt('formatting'); needsCanvasUpdate = true">Formatting: {{ options.formatting }}</button></td>
+	</table>`
     },
 
     'back-button': {
