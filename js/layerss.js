66170f9ef00235ce2d9e764fff5b68814d2740b6
Askinga <152022166+Askinga@users.noreply.github.com>
2024-09-19 19:56:44 -0400

    Update layerss.js

--- js/layerss.js
+++ js/layerss.js
@@ -38,6 +38,30 @@ addLayer("SCH", {
 	}
     },
     branches:["SAC", "GLA"],
+    tabFormat: {
+        "Upgrades": {
+            content: [
+                "main-display",
+                "resource-display",
+                "prestige-button",
+                "blank",
+                "upgrades"
+            ],
+        },
+	"*Students*": {
+          unlocked() { return (hasUpgrade('SCH', 25)) },
+	    content: [
+                "main-display",
+		"blank",
+		["display-text",
+				function() {return 'You have ' + format(player.SCH.students) + ' MJ Students (based on MJs), which are boosting MJs by '+'^'+format(tmp.SCH.powerEff)+(hasUpgrade('p', 46)?" (Your super points are also boosting Upgrade Points by "+format(tmp.p.powerEff)+")":"")},
+					{}],
+		"resource-display",
+                "prestige-button",
+                "blank",
+                "clickables"
+            ],
+        },
     upgrades: {
         11: {
             title: "Everything Reset!!!!",
