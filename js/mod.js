0cd3a9afdd67bf65127d7e0d84bb6fd9ae1862c2
Askinga <152022166+Askinga@users.noreply.github.com>
2024-10-05 23:14:48 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -101,6 +101,7 @@ function getPointGen() {
 	if (hasUpgrade('SCH', 14)) gain = gain.pow(1.1)
 	if (hasUpgrade('SCH', 15)) gain = gain.times("e5000000")
 	if (hasUpgrade('SCH', 22)) gain = gain.times(upgradeEffect('SCH', 22))
+	if (hasUpgrade('SCH', 31)) gain = gain.times("e15000000")
 	gain = gain.times(tmp.SCH.powerEff)
 	if (inChallenge('SAC', 21)) gain = gain.pow(0)
 	return gain
