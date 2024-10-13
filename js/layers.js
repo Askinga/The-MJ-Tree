3255247bf1d3a09cc6edfc33c57d3d0a44d9e337
Askinga <152022166+Askinga@users.noreply.github.com>
2024-10-12 22:49:08 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -104,36 +104,36 @@ addLayer("p", {
     upgrades: {
 	11: {
             title: "The first upgrade",
-	    description: function() {return `<br>×2 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.1 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
+	    description: function() {return `<br>×2 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.1 <span style=\"color: rgb(255, 0, 0); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
 	    cost: new Decimal(1),
 	},
         12: {
             title: "The second upgrade",
-	    description: function() {return `<br>×1.5 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.2 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
+	    description: function() {return `<br>×1.5 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.2 <span style=\"color: rgb(255, 0, 0); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
 	    cost: new Decimal(3),
 	    unlocked() { return (hasUpgrade('p', 11)) }
 	},
         13: {
             title: "The third upgrade",
-	    description: function() {return `<br>×2 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.3 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
+	    description: function() {return `<br>×2 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.3 <span style=\"color: rgb(255, 0, 0); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
 	    cost: new Decimal(5),
 	    unlocked() { return (hasUpgrade('p', 12)) }
 	},
         14: {
             title: "The forth upgrade",
-	    description: function() {return `<br>×2.5 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.4 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
+	    description: function() {return `<br>×2.5 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.4 <span style=\"color: rgb(255, 0, 0); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
 	    cost: new Decimal(12),
 	    unlocked() { return (hasUpgrade('p', 13)) }
 	},
   	15: {
             title: "The fifth upgrade",
-	    description: function() {return `<br>×3 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.5 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
+	    description: function() {return `<br>×3 <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> and ×1.5 <span style=\"color: rgb(255, 0, 0); text-shadow: rgb(255, 0, 0) 0px 0px 10px;\">prestige points</span>`},
 	    cost: new Decimal(30),
 	    unlocked() { return (hasUpgrade('p', 14)) }
 	},
         21: {
             title: "The Sixth Upgrade",
-            description: function() {return `<br> Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\"><h3>points</h3></span><br> based on points `},
+            description: function() {return `<br> Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\"><h3>points</h3></span><br> based on <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br>`},
             cost: new Decimal(100),
             effect(){
                 let expu3 = 0.125
@@ -154,7 +154,7 @@ addLayer("p", {
         },
         22: {
             title: "The Seventh Upgrade",
-            description: function() {return `<br>Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> based on points (weaker 1)`},
+            description: function() {return `<br>Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\"><h3>points</h3></span><br> based on <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> (weaker 1)`},
             cost: new Decimal(250),
             effect(){
                 let expu3 = 0.110
@@ -175,7 +175,7 @@ addLayer("p", {
         },
         23: {
             title: "The Eighth Upgrade",
-            description: function() {return `<br>Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> based on points (weaker 2)`},
+            description: function() {return `<br>Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\"><h3>points</h3></span><br> based on <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> (weaker 2)`},
             cost: new Decimal(400),
             effect() {
                 return player.points.add(1).pow(0.095)
@@ -186,7 +186,7 @@ addLayer("p", {
         },
         24: {
             title: "The Ninth Upgrade",
-            description: function() {return `<br> Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> based on points (weaker 3)`},
+            description: function() {return `<br> Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\"><h3>points</h3></span><br> based on <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> (weaker 3)`},
             cost: new Decimal(750),
             effect() {
                 return player.points.add(1).pow(0.08)
@@ -197,7 +197,7 @@ addLayer("p", {
         },
         25: {
             title: "The Tenth Upgrade",
-            description: function() {return `<br> Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> based on points (weaker 4)`},
+            description: function() {return `<br> Boost your <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\"><h3>points</h3></span><br> based on <span style=\"color: rgb(255, 255, 255); text-shadow: rgb(255, 255, 255) 0px 0px 10px;\">points</span><br> (weaker 4)`},
             cost: new Decimal(1500),
             effect(){
     	    let rpow = 0.065
