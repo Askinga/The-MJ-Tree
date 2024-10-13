8e2d76b5b3b70782576ebfbebf73b16bb163fd93
Askinga <152022166+Askinga@users.noreply.github.com>
2024-10-13 09:20:34 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,16 +13,16 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "2.2.0",
-	name: "Sanas challenges?",
+	num: "2.3.0",
+	name: "It's time to learn",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v2.2.0</h3><br>
-		- More updates soon! Endgame: Beat Sanas Challenge 10.<br>
-		- Added Sanas challenges, Added 10 challenges, Added 4 achievements. Some challenges have you wait, grind and tetrate your MJ gain!<br>
+	<h3>v2.3.0</h3><br>
+		- More updates soon! Endgame: MJ School Upgrade 35.<br>
+		- Added MJ Schools, Added 15 upgrades, Added MJ Students, Added 3 achievements, Added 2 savebanks, Microtabs are a better way to sort out things. For now, it will be added in v2.3.0+. A specific MJ School upgrades makes hyper MJs boost normal MJs! And minor bug fixes<br>
                 - Check back here for more updates! Full changelog at <a href=https://galaxy.click/updates/355>galaxy.click/updates/355</a>`
-  
+
 let winText = `Congratulations! You have reached the end and beaten this game. Good job! 🏆`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
@@ -114,12 +114,12 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
-    "Beat Sanas Challenge 10 to beat the game!"  
+    "Get MJ School Upgrade 35 to beat the game!"  
 ]
 
 // Determines when the game "ends"
 function isEndgame() {
-	return (hasChallenge('SAC', 21))
+	return (hasUpgrade('SCH', 35))
 }
 
 
