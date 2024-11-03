beb4746cfca5d7a1fa46cc11181f2ff88ade9da4
Askinga <152022166+Askinga@users.noreply.github.com>
2024-11-03 10:16:41 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The MJ Tree REWRITTEN",
-	id: "mymod1",
+	name: "The Math Tree",
+	id: "mymod1MATH",
 	author: "Askinga",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
@@ -13,49 +13,13 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.4 Universes",
+	num: "0.0",
 	name: "Not Finished!",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-                <h3>v0.5</h3><br>
-		- In the works.<br>
-		
-  
-                <h3>v0.4</h3><br>
-                - Added a new layer called MJ Universes.<br>
-		- Added 2 achievements.<br>
-		- Added 1 upgrade.<br>
-                - Added 2 buyables.<br>
-		- Added 13 milestones.<br>
-		- Endgame: MJ Universe Upgrade 1.<br>
-		
-
-                <h3>v0.3</h3><br>
-                - Added Skill Mastery.<br>
-		- Added 1 achievement.<br>
-		- Added 5 upgrades.<br>
-		- Added Milestones.<br>
-                - Added 2 milestones.<br>
-		- Endgame: Skill Mastery Milestone 2.<br>
-		
-  
-                <h3>v0.2</h3><br>
-                - Added Achievements.<br>
-		- Added 8 achievements.<br>
-		- Added 10 upgrades.<br>
-		- Endgame: MJ Worlds Upgrade 15.<br>
-
-  
-                <h3>v0.1</h3><br>
-		- Added 3 layers: MJs, MJ Buses and MJ Worlds.<br>
-                - Added 25 upgrades.<br>
-		- Endgame: MJ Worlds Upgrade 10.<br>
-
-		
-		<h3>v0.0</h3><br>
-		- First version.<br>
-                - Nothing special.`
+                
+                - Nothing!`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
@@ -78,40 +42,6 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	
-	// add
-	
-	gain = gain.add(layers.p.effect())
-	if (hasUpgrade('p', 12)) gain = gain.add(upgradeEffect('p', 12))
-	
-	// times
-	
-	if (hasUpgrade('p', 11)) gain = gain.times(1.5)
-	if (hasUpgrade('p', 13)) gain = gain.times(upgradeEffect('p', 13))
-	if (hasUpgrade('p', 15)) gain = gain.times(2)
-	if (hasUpgrade('b', 12)) gain = gain.times(3.141592653)
-	gain = gain.times(layers.b.effect())
-	if (hasUpgrade('p', 23)) gain = gain.times(2.5)
-	if (hasUpgrade('w', 11)) gain = gain.times(10)
-	if (hasUpgrade('w', 34)) gain = gain.times(50)
-	if (hasMilestone('m', 0)) gain = gain.times(100)
-	if (hasMilestone('m', 6)) gain = gain.times(1e10)
-	if (hasMilestone('m', 11)) gain = gain.times(1e20)
-	if (hasAchievement('🏆', 25)) gain = gain.times(1e15)
-	if (hasUpgrade('u', 14)) gain = gain.times(upgradeEffect('u', 14))
-	if (hasUpgrade('u', 22)) gain = gain.times(1e12)
-	if (hasUpgrade('u', 25)) gain = gain.times(1e21)
-	if (hasUpgrade('u', 31)) gain = gain.times(upgradeEffect('u', 31))
-	
-	// power
-	
-	if (hasUpgrade('b', 24)) gain = gain.pow(1.03)
-	if (hasUpgrade('w', 32)) gain = gain.pow(1.02)
-	if (hasUpgrade('p', 32)) gain = gain.pow(1.025)
-	if (hasMilestone('m', 0)) gain = gain.pow(1.02)
-	if (hasMilestone('m', 4)) gain = gain.pow(1.1)
-	if (hasMilestone('m', 8)) gain = gain.pow(1.15)
-	if (hasMilestone('m', 13)) gain = gain.pow(1.02)
 	return gain
 }
 
@@ -125,7 +55,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return (hasUpgrade('u', 33))
+	return (player.points.gte("ee20")
 }
 
 
