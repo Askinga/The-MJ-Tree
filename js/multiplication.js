a1477e8bb23504633fbac1c299c42c4dbff49c8d
Askinga <152022166+Askinga@users.noreply.github.com>
2024-11-04 08:01:21 -0500

    Update multiplication.js

--- js/multiplication.js
+++ js/multiplication.js
@@ -1,1 +1,53 @@
-// nothing here yet...
+addLayer("m", {
+    name: "Multiplication",
+    symbol: "✖️",
+    position: 1,
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#a8a8a8",
+    requires() {
+        let req = new Decimal("eeeeeeeeeeeeee10")
+	return req
+    }, // Can be a function that takes requirement increases into account
+    resource: "Multiplication", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1.65, // Prestige currency exponent
+    effect(){
+    let rpow = 1
+	let eff = player.m.points.pow(rpow)
+       return eff
+        },
+        effectDescription() {
+            let desc = "multiplying points by " + format(tmp[this.layer].effect) + "x";
+            return desc;
+        },
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+	return mult
+    },
+
+
+
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    displayRow: 1,
+    hotkeys: [
+        {key: "m", description: "A: Reset for Multiplication", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    resetDescription: 'Reset points and Addition for ',
+    layerShown(){
+        let visible = false
+        if (hasMilestone('c', 5) || player.m.unlocked) visible = true
+       return visible
+    },
+    branches:["c"],
+    tooltip() { // Optional, tooltip displays when the layer is locked
+        return ("Multiplication")
+    },
+})
