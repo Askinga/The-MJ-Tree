a032a06bb0a5ffc2b512e3d27fe656e305e33d85
Askinga <152022166+Askinga@users.noreply.github.com>
2024-11-10 21:26:30 -0500

    Update computing.js

--- js/computing.js
+++ js/computing.js
@@ -171,7 +171,7 @@ componentStyles: {
 	},
         12: {
             name: "Challenge 2: Slowdown",
-            challengeDescription() {return `Points divide point gain.<br>Currently: /${format(player.points.pow(0.3))}`},
+            challengeDescription() {return `Points divide point gain.<br>Currently: /${format(player.points.pow(0.25))}`},
             canComplete: function() {return player.d.points.gte("1")},
             goalDescription: "Get 1 Division.",
             rewardDescription: "10x points per challenge completed.",
