7c1f5d105235a129c94ac4d92da7366400690fc6
Askinga <152022166+Askinga@users.noreply.github.com>
2024-11-14 17:49:32 -0500

    Update computing.js

--- js/computing.js
+++ js/computing.js
@@ -25,6 +25,14 @@ componentStyles: {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    update(delta) {
+        let onepersec = new Decimal(1)
+
+        if (player.startedGame == false && player.points.gte(1))
+        {
+            player.startedGame = true
+	}
+    },
     exponent() {
         let exp = 1.625
         return exp
