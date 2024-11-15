ad4eef8d2c9353b92b53a7ac6ad262d657fca23a
Askinga <152022166+Askinga@users.noreply.github.com>
2024-11-15 08:22:24 -0500

    Update computing.js

--- js/computing.js
+++ js/computing.js
@@ -34,7 +34,7 @@ componentStyles: {
         {
             player.startedGame = true
 	}
-        if (player.startedGame && !inChallenge('c', 11) || inChallenge('c', 12) || inChallenge('c', 13))
+        if (player.startedGame && !(inChallenge('c', 11) || inChallenge('c', 12) || inChallenge('c', 13)) )
         {
             playAndLoopAudio("music/And-So-It-Begins-Inspired-By-Crush-Sometimes(chosic.com).mp3", options.musicVolume/8);
 	}
