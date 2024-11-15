8be81cd274a9e74d35be51d6fd0547512195186a
Askinga <152022166+Askinga@users.noreply.github.com>
2024-11-15 08:35:43 -0500

    Update computing.js

--- js/computing.js
+++ js/computing.js
@@ -38,9 +38,9 @@ componentStyles: {
         if (player.c.startedGame && !(inChallenge('c', 11) || inChallenge('c', 12) || inChallenge('c', 13)) )
         {
             playAndLoopAudio("music/And-So-It-Begins-Inspired-By-Crush-Sometimes(chosic.com).mp3", options.musicVolume/10);
-	} else if (player.c.startedGame && (inChallenge("c", 11) || inChallenge("c", 12) || inChallenge("c", 13)) && options.musicToggle)
+	} else if (player.c.startedGame && (inChallenge("c", 11) || inChallenge("c", 12) || inChallenge("c", 13)) )
         {
-            playAndLoopAudio("music/tav.mp3", options.musicVolume/10);
+            playAndLoopAudio("music/maxkomusic-heroism(chosic.com).mp3", options.musicVolume/10);
 	}
     },
     exponent() {
