823fb2e31ec742fb9881dc28aaa1e438c3f9bb07
tmt-master <maah6928@rscloud.ca>
2024-11-15 13:03:05 -0500

    Update p1.js

--- js/p1.js
+++ js/p1.js
@@ -16,7 +16,7 @@ addLayer("p1", {
     resource: "points^1", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
      passiveGeneration() {   // Also the amount required to unlock the layer.
-    	return player.p2.effect
+    	return layers.p2.effect
     },
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
