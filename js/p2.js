9a45699f4c8c9a9ac820e0d005f5c46f59579843
tmt-master <maah6928@rscloud.ca>
2024-11-15 13:22:44 -0500

    Update p2.js

--- js/p2.js
+++ js/p2.js
@@ -6,7 +6,7 @@ addLayer("p2", {
 
     color: "#e8e8e8",                       // The color for this layer, which affects many elements.
     resource: "points^2",            // The name of this layer's main prestige resource.
-    row: 0,                                 // The row this layer is on (0 is the first row).
+    row: 1,                                 // The row this layer is on (0 is the first row).
 
     baseResource: "points^1",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.p1.points },  // A function to return the current amount of baseResource.
