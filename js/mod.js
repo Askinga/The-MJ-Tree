3600fc75a77b61711f1980504be823a95691b3cf
Askinga <152022166+Askinga@users.noreply.github.com>
2024-12-25 18:42:22 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -12,13 +12,18 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Prestiged",
 }
 
-let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
+let changelog = `<h1>Changelog:</h1><br/>
+	<h3>v0.1</h3><br/>
+		- Added Prestige layer.<br/>
+  		- Added 10 new upgrades.<br/>
+    		- Added Booster layer. (not finished)<br/>
+ 		<br/>
+ 	<h3>v0.0</h3><br/>
+		- Added things.<br/>
 		- Added stuff.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
