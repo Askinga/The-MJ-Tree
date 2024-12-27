39416655ec65ba66c94b00ace70262d13a3f960e
Askinga <152022166+Askinga@users.noreply.github.com>
2024-12-27 16:22:40 -0500

    Create rebirth.js

---
+++ js/rebirth.js
@@ -0,0 +1,36 @@
+addLayer("r", {
+    name: "Rebirth",
+    symbol: "R",
+    position: 0,
+    startData() { return {                  // startData is a function that returns default data for a layer. 
+        unlocked: false,                     // You can add more variables here to add them to your layer.
+        points: new Decimal(0),         // "points" is the internal name for the main resource of the layer.
+    }},
+    color: "#007bff",                       // The color for this layer, which affects many elements.
+    resource: "rebirth points",            // The name of this layer's main prestige resource.
+    row: 2,                                 // The row this layer is on (0 is the first row).
+    baseResource: "points",                 // The name of the resource your prestige gain is based on.
+    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+    requires(){
+	let req = new Decimal(1e24)
+	return req
+    },              // The amount of the base needed to gain 1 of the prestige currency.                                    // Also the amount required to unlock the layer
+    type: "normal",                         // Determines the formula used for calculating prestige currency.
+    exponent: 0.1,                          // "normal" prestige gain is (currency^exponent).
+    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
+        mult = new Decimal(1)               // Factor in any bonuses multiplying gain here.
+    	return mult
+    },
+    gainExp() {                             // Returns the exponent to your gain of the prestige resource.
+        return new Decimal(1)
+    },
+    layerShown(){
+        let visible = false
+        if (hasUpgrade('g', 25) || player.r.unlocked) visible = true
+       return visible
+},          // Returns a bool for if this layer's node should be visible in the tree.
+    branches: ["g"],
+    hotkeys: [
+        {key: "r", description: "R: Reset for rebirth points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+})
