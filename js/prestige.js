20684e6b02ef186d6a8e30b13a9b51505bb7c8ee
Askinga <152022166+Askinga@users.noreply.github.com>
2024-12-27 19:26:49 -0500

    Update prestige.js

--- js/prestige.js
+++ js/prestige.js
@@ -11,6 +11,11 @@ addLayer("p", {
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
+    passiveGeneration(){
+	gen = new Decimal(0)
+	    if(hasUpgrade('r', 12)) gen = gen.add(0.5)
+	return gen
+    },
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -35,7 +40,7 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    autoUpgrade(){return (hasUpgrade('g', 23))},
+    autoUpgrade(){return (hasUpgrade('g', 23) || hasUpgrade('r',12))},
     layerShown(){return true},
     upgrades: {
         11: {
