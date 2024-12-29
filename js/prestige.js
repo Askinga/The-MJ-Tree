3b17af194908ce0b9f88bd2d7978dc6f73585889
Askinga <152022166+Askinga@users.noreply.github.com>
2024-12-28 23:21:08 -0500

    Update prestige.js

--- js/prestige.js
+++ js/prestige.js
@@ -36,6 +36,7 @@ addLayer("p", {
 	if(hasUpgrade('r',25)) mult = mult.times(5)
 	if(hasUpgrade('r',31)) mult = mult.times(5)
 	if(hasUpgrade('r',32)) mult = mult.times(2.5)
+	if(hasUpgrade('r',34)) mult = mult.times(10)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
