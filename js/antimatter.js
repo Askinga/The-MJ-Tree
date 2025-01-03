6f91c9ef08f9f2d3bb911ef3e3c649c918fbc73e
Askinga <152022166+Askinga@users.noreply.github.com>
2025-01-02 23:48:33 -0500

    Update antimatter.js

--- js/antimatter.js
+++ js/antimatter.js
@@ -23,7 +23,7 @@ addLayer("a", {
     baseResource: "points",                 // The name of the resource your prestige gain is based on.
     baseAmount() { return player.points },  // A function to return the current amount of baseResource.
     requires(){
-	  let req = new Decimal("eeeeeeeeee2000")
+	  let req = new Decimal(1e100)
     return req
     },              // The amount of the base needed to gain 1 of the prestige currency.                                    // Also the amount required to unlock the layer
     type: "normal",   
