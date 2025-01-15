b73f50d22bdd5d3f4735bfeec23ac7204b69140e
Askinga <152022166+Askinga@users.noreply.github.com>
2025-01-15 16:31:38 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Trove Tree",
+	author: "Askinga",
+	pointsName: "strength",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
