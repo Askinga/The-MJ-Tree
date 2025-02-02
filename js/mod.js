256e8a960d075a68b9e2b40e0806ea357fcce317
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-02 12:47:42 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -13,7 +13,7 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.4.0",
+	num: "0.4.1",
 	name: "Infinite... Part 1",
 }
 
@@ -63,7 +63,7 @@ let changelog = `<h1>Changelog:</h1><br>
 		- Added things.<br>
 		- Added stuff.`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have reached the end and beaten this game, but thats it for now...`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
