9edfec6e11c18e957ab5f088df19266dde814c35
Askinga <152022166+Askinga@users.noreply.github.com>
2025-02-02 11:37:40 -0500

    Create levels.js

---
+++ js/levels.js
@@ -0,0 +1,45 @@
+addLayer("l", {
+    name: "levels", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "L", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+	        total: new Decimal(0),
+	        best: new Decimal(0),
+	    	xp: new Decimal(0),
+        xpg: new Decimal(0)
+    }},
+    color: "#8aff8a",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "Levels", // Name of prestige currency
+    baseResource: "XP", // Name of resource prestige is based on
+    baseAmount() {return player.l.xp}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 1.25, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    canReset(){return false},
+    layerShown(){return hasMilestone('pr',4)},
+    effect(){
+	let base = 1.2
+	let eff = new Decimal(base).pow(player.l.points)
+       return eff
+       },
+        effectDescription() {
+            let desc = "which is boosting Strength generation by x" + format(tmp[this.layer].effect);
+            return desc;
+        },
+        tabFormat: [
+        "main-display",
+        "prestige-button",
+        "resource-display",
+    ],
+  branches: ["pr"],
+})
