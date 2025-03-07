38f293b9e13f7a1b5576a4f6fe2133aeab93e692
Askinga <musicgemca@gmail.com>
2025-03-07 14:06:12 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -92,7 +92,7 @@ addLayer("p", {
 	    return player[this.layer].points.add(1).pow(0.25)
 	},
 	effectDisplay(){
-	    return format(upgradeEffect(this.layer, this.id))
+	    return format(upgradeEffect(this.layer, this.id))+'x'
 	},
 	unlocked(){ return (hasUpgrade('p', 15))}
       },
