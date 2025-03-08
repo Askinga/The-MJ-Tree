c8361c68225c77c82e817c8d051f158a032bea7d
Askinga <musicgemca@gmail.com>
2025-03-08 12:52:18 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -54,6 +54,7 @@ function getPointGen() {
 	if (hasUpgrade('s', 12)) gain = gain.times(3)
 	if (hasUpgrade('s', 13)) gain = gain.times(4)
 	if (hasUpgrade('s', 21)) gain = gain.times(2)
+	if (hasUpgrade('s', 23)) gain = gain.times(upgradeEffect('s', 23))
 	return gain
 }
 
