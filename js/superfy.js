8213181186368e7937d3ebcd69495e848240103a
Askinga <musicgemca@gmail.com>
2025-03-07 20:57:28 -0500

    Update superfy.js

--- js/superfy.js
+++ js/superfy.js
@@ -7,6 +7,7 @@ addLayer("s", {
 	}, else {
 		sym = "S"
 	},
+	return sym
     },// This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
