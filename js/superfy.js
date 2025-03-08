18ba65b7856a5d99dc3b34e9ab32d1e2546f87d7
Askinga <musicgemca@gmail.com>
2025-03-08 14:31:02 -0500

    Update superfy.js

--- js/superfy.js
+++ js/superfy.js
@@ -99,6 +99,18 @@ addLayer("s", {
 	cost: new Decimal(125),
 	unlocked(){ return (hasUpgrade('s', 23))}
       },
+      25: {
+	title: "Super Growing",
+	description: "Boost prestige points based on super.",
+	cost: new Decimal(500),
+	effect(){
+	    return player[this.layer].points.add(1).pow(0.3)
+	},
+	effectDisplay(){
+	    return format(upgradeEffect(this.layer, this.id))+'x'
+	},
+	unlocked(){ return (hasUpgrade('s', 24))}
+      },
     },
     milestones: {
         0: {
