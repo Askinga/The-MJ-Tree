e718b7a9c34c22e1e1367af299fc8d3904f4d52e
Askinga <musicgemca@gmail.com>
2025-03-07 20:58:02 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,6 +7,7 @@ addLayer("p", {
 	}, else {
 		sym = "P"
 	},
+	return sym
     }, // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
