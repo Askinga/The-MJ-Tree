98483ab512e82e60b7f717f586982b891ae1dfb2
Askinga <musicgemca@gmail.com>
2025-03-08 23:48:35 -0500

    Update mega.js

--- js/mega.js
+++ js/mega.js
@@ -136,7 +136,7 @@ addLayer("m", {
 	    return player[this.layer].points.add(1).pow(0.75)
 	},
 	effectDisplay(){
-	    return '^'+format(upgradeEffect(this.layer, this.id))
+	    return format(upgradeEffect(this.layer, this.id))+'x'
 	},
 	unlocked(){ return (hasUpgrade('m', 31))}
       },
