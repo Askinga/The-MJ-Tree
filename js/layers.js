4045086df36b90401a059fb29a58b8c1680cb0db
Askinga <musicgemca@gmail.com>
2025-03-09 12:20:00 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -260,7 +260,7 @@ addLayer("p", {
                 return new Decimal(30).mul(Decimal.pow(1.25, x)).mul(Decimal.pow(x , Decimal.pow(exp2 , x))).floor()
             },
             display() {
-                return "Boost points by ^1.01 per level. (compounding) " + "<br>Cost: " + format(tmp[this.layer].buyables[this.id].cost) + " prestige points" + "<br>Level " + getBuyableAmount(this.layer, this.id) + "<br>Effect: Boost Point gain by ^" + format(buyableEffect(this.layer, this.id))
+                return "Boost points by ^1.01 per level. (compounding) " + "<br>Cost: " + format(tmp[this.layer].buyables[this.id].cost) + " prestige points" + "<br>Level " + getBuyableAmount(this.layer, this.id) + "/100" + "<br>Effect: Boost Point gain by ^" + format(buyableEffect(this.layer, this.id))
             },
             canAfford() {
                 return player[this.layer].points.gte(this.cost())
@@ -279,7 +279,8 @@ addLayer("p", {
             },
             tooltip() {
                 return "Cost Formula: 30 x 1.25^Amt x Amt^(" + exp2 + "^Amt). Effect formula: " + format(base1) + "^(" + format(base2) + "^" + expo + ")."
-            }
+            },
+	    purchaseLimit(){ return new Decimal(100)}
         },
     },
 })
