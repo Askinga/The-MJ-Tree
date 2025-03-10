d3d09a9be5b71b8dbba3551d890b51e8c2ac7b48
Askinga <musicgemca@gmail.com>
2025-03-09 22:58:39 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The Prestige Zen",
 	author: "Askinga",
 	pointsName: "points",
-	modFiles: ["layers.js", "superfy.js", "mega.js", "achievements.js", "tree.js"],
+	modFiles: ["layers.js", "superfy.js", "mega.js", "ultra.js", "achievements.js", "tree.js"],
 
 	discordName: "",
 	discordLink: "",
