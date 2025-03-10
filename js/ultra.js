fa4bdece0b0e5c125ffad3383611b7d9f4b1c760
Askinga <musicgemca@gmail.com>
2025-03-09 22:56:44 -0400

    Create ultra.js

---
+++ js/ultra.js
@@ -0,0 +1,38 @@
+addLayer("u", {
+    name: "Ultra", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol(){
+        if (options.layerEmojis == true) symbol = "⚛️"
+        else symbol = "U"
+        return symbol
+    }, // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#006aff",
+    requires: new Decimal(20000), // Can be a function that takes requirement increases into account
+    resource: "Ultra", // Name of prestige currency
+    baseResource: "Mega", // Name of resource prestige is based on
+    baseAmount() {return player.m.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.2, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 3, // Row the layer is in on the tree (0 is the first row)
+    resetDescription(){
+          return 'Ultrafy for '
+    },
+		tooltip(){
+      return 'Ultrafy'
+    },
+    hotkeys: [
+        {key: "u", description: "U: Ultrafy.", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return (hasUpgrade('m', 45) || player.u.unlocked)}
+})
