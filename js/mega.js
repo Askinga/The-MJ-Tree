d181083ad86ecd779a98586e7df0ebbff432e889
Askinga <musicgemca@gmail.com>
2025-03-14 22:35:31 -0400

    Update mega.js

--- js/mega.js
+++ js/mega.js
@@ -162,13 +162,14 @@ addLayer("m", {
 	description: "Boost the Mega Effect based on points.",
 	cost: new Decimal(400),
 	effect(){
-	    let eff2 = new Decimal(0.001)
+	    let eff2 = 0.001
 	    let eff player.points.add(1).pow(eff2)
 	    eff = softcap(eff, new Decimal("1.25"), 0)
 	    return eff
 	},
 	effectDisplay(){
 	    let softcapDescription = ''
+	    let upgEffect = upgradeEffect(this.layer, this.id)
 	    if (upgEffect.gte(new Decimal("1.25")) ) {
                     softcapDescription = " (HARDCAPPED)"
 	    }
