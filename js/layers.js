8936085fb24e9ff5cb555f9e1188cd62db9dc2aa
Askinga <musicgemca@gmail.com>
2025-03-31 13:42:19 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -754,6 +754,7 @@ addLayer("m", {
     resource: "mastered skills", // Name of prestige currency
     baseResource: "MJs", // Name of resource prestige is based on
     baseAmount() {return player.p.points}, // Get the current amount of baseResource
+    milestonePopups(){ return options.milestonePopups },
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 2.825, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
