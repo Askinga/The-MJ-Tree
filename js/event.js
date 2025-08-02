911f1ae65ec8358afcae8709cfd482c1de9347cc
Askinga <musicgemca@gmail.com>
2025-08-02 16:44:28 -0400

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,32 +1,32 @@
 addLayer("event", {
-    name: "Events", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    name: "Events",
+    symbol: "E",
+    position: 0,
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+        points: new Decimal(0),
     }},
     tooltip(){
       return "Events!"
     },
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
+    requires: new Decimal(10),
+    resource: "prestige points",
+    baseResource: "points",
+    baseAmount() {return player.points},
+    type: "normal",
+    exponent: 0.5,
+    gainMult() {
+        let mult = new Decimal(1)
         return mult
     },
-    gainExp() { // Calculate the exponent on main currency from bonuses
+    gainExp() {
         return new Decimal(1)
     },
-    tabFormat: {
+    tabFormat: [
       ["display-text", function(){ return "A new update, coming soon." }],
-      ["raw-html", function () { return "<img src='resources/update.png'</img>"  }],
-    },
-    row: "side", // Row the layer is in on the tree (0 is the first row)
+      ["raw-html", function () { return "<img src='resources/update.png'>"  }],
+    ],
+    row: "side", // Only if "side" is valid in your framework
     layerShown(){return true}
 })
