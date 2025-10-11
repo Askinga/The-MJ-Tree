ac885caf6791e3609952adf4eb6de64720a75fe0
Askinga <musicgemca@gmail.com>
2025-10-11 09:45:43 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -79,7 +79,7 @@ addLayer("d", {
     },
 	13: {
 		title: "HARD",
-        display() {return "Hard mode: /3 all resources and /3.33 pointa"},
+        display() {return "Hard mode: /3 all resources and /3.33 points"},
         canClick(){ return true },
 		onClick(){ 
 			player.d.difficulty = new Decimal(2)
