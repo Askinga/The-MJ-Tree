add26b7c52aff2c1ea189b47cdedb4c26026e0e4
Askinga <musicgemca@gmail.com>
2025-10-11 09:43:08 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,44 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+let easy = {
+  width: "100px",
+  height: "100px",
+  "min-height": "120px",
+  "font-size": "10px",
+  margin: "10px",
+  "border-radius": "33%",
+  "background-color": "#00aaff"
+};
+let normal = {
+  width: "100px",
+  height: "100px",
+  "min-height": "120px",
+  "font-size": "10px",
+  margin: "10px",
+  "border-radius": "33%",
+  "background-color": "#00aa00"
+};
+let hard = {
+  width: "100px",
+  height: "100px",
+  "min-height": "120px",
+  "font-size": "10px",
+  margin: "10px",
+  "border-radius": "33%",
+  "background-color": "#aa0000"
+};
+
+addLayer("d", {
+    name: "Difficulty", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "D", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
+		difficulty: new Decimal(1),
+		started: new Decimal(0),
     }},
     color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    requires: new Decimal("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee10"), // Can be a function that takes requirement increases into account
+    resource: "Difficulty", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -20,9 +50,46 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
-    layerShown(){return true}
+	tabFormat: [
+		["display-text", "<h2>Select difficulty...</h2>"],
+		"blank",
+		"clickables"
+	],
+    row: "side", // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true},
+	clickables: {
+    11: {
+		title: "EASY",
+        display() {return "Easy mode: x2 all resources and points and ^1.01 points"},
+        canClick(){ return true },
+		onClick(){ 
+			player.d.difficulty = new Decimal(0)
+		},
+		style(){
+		   return { ...easy }
+		},
+    },
+	12: {
+		title: "NORMAL",
+        display() {return "Normal mode: Normal TMT game"},
+        canClick(){ return true },
+		onClick(){ 
+			player.d.difficulty = new Decimal(1)
+		},
+		style(){
+		   return { ...normal }
+		},
+    },
+	13: {
+		title: "HARD",
+        display() {return "Hard mode: /3 all resources and /3.33 pointa"},
+        canClick(){ return true },
+		onClick(){ 
+			player.d.difficulty = new Decimal(2)
+		},
+		style(){
+		   return { ...hard }
+		},
+    },
+	},
 })
