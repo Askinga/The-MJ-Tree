c42dd16028cf5fb5a9227f5cc4e6288458e06652
Askinga <musicgemca@gmail.com>
2025-10-14 17:14:31 -0400

    Create super.js

---
+++ js/super.js
@@ -0,0 +1,31 @@
+addLayer("s", {
+    name: "super runes", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "SR", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#4BDC13",
+    requires: new Decimal(2500), // Can be a function that takes requirement increases into account
+    resource: "Super Runes", // Name of prestige currency
+    baseResource: "Common Runes", // Name of resource prestige is based on
+    baseAmount() {return player.p.common}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "s", description: "s: Reset for super runes", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return (hasUpgrade('p', 321) || player.s.unlocked)},
+    branches: ["p"],
+    effect(){ return player.s.points.add(1).pow(2) },
+    effectDescription(){ return "which is boosting Rune gain by x" + format(tmp.s.effect) },
+})
