694eea00bc75ab8142900678732b6301ab3ef708
Askinga <musicgemca@gmail.com>
2025-10-15 19:53:45 -0400

    Update requirements and exponent for Super Runes

--- js/super.js
+++ js/super.js
@@ -7,12 +7,12 @@ addLayer("s", {
 		points: new Decimal(0),
     }},
     color: "#dea8ff",
-    requires: new Decimal(2500), // Can be a function that takes requirement increases into account
+    requires: new Decimal(10000), // Can be a function that takes requirement increases into account
     resource: "Super Runes", // Name of prestige currency
     baseResource: "Common Runes", // Name of resource prestige is based on
     baseAmount() {return player.p.common}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.1, // Prestige currency exponent
+    exponent: 0.075, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
 		if (player.d.difficulty.eq(0)) mult = mult.times(2)
