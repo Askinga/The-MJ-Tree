babb685aa29dfbbab2fddbd0fb566815a6518a96
Askinga <musicgemca@gmail.com>
2025-10-17 07:24:56 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "Rune Tree",
 	author: "Askinga/Sanas",
 	pointsName: "points",
-	modFiles: ["layers.js", "tree.js", "prestige.js", "super.js", "boost.js"],
+	modFiles: ["layers.js", "tree.js", "prestige.js", "super.js", "boost.js", "meta.js"],
 
 	discordName: "",
 	discordLink: "",
