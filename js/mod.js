d64a0d5ffce1239d64b34b579ea2f8388096dd68
Askinga <musicgemca@gmail.com>
2025-10-20 14:25:33 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -58,6 +58,7 @@ function getPointGen() {
 	gain = gain.times(layers.m.effect())
 	gain = gain.times(tmp.l.lvlE2)
 	if (hasUpgrade('u', 11)) gain = gain.times("e100")
+	if (hasUpgrade('u', 12)) gain = gain.times("e50")
 	if (player.d.difficulty.eq(0)) gain = gain.pow(1.01)
 	return gain
 }
