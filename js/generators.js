bf249e4f04615ce2457ae8e0637b2c42f27c58ba
Askinga <musicgemca@gmail.com>
2025-11-04 19:45:34 -0500

    Update generators.js

--- js/generators.js
+++ js/generators.js
@@ -19,6 +19,7 @@ addLayer("XP", {
 		if (hasMilestone('XP', 2)) mult = mult.times(tmp.XP.boost2)
 		if (hasMilestone('XP', 4)) mult = mult.times(5)
 		if (hasMilestone('XP', 5)) mult = mult.times(10)
+		if (hasMilestone('XP', 8)) mult = mult.times(20)
         return mult
     },
     tabFormat: [
@@ -33,7 +34,9 @@ addLayer("XP", {
         return new Decimal(1)
     },
     gen(){
-      let eff = player.XP.points.pow(2)
+	  let pow = new Decimal(2)
+	  if (hasMilestone('XP', 8)) pow = pow.add(1)
+      let eff = player.XP.points.pow(pow)
 	  if (hasMilestone('XP', 1)) eff = eff.times(tmp.XP.boost1)
 	  if (hasMilestone('XP', 6)) eff = eff.times(tmp.XP.boost3)
 	  return eff
@@ -117,6 +120,16 @@ addLayer("XP", {
         requirementDescription: "1,000,000 XP Boosters",
         effectDescription(){ return "Boost 2nd Level Effect Base based on XP Boosters. Currently: x" + format(tmp.XP.boost4) },
         done() { return player.XP.points.gte(1e6) }
+    },
+	8: {
+        requirementDescription: "1,000,000 XP Boosters",
+        effectDescription(){ return "Boost 2nd Level Effect Base based on XP Boosters. Currently: x" + format(tmp.XP.boost4) },
+        done() { return player.XP.points.gte("1e6") }
+    },
+	8: {
+        requirementDescription: "2,500,000 XP Generators",
+        effectDescription(){ return "Boost XP Generators by x20 and +1 XP Generator effect power base." },
+        done() { return player.XP.points.gte("2.5e6") }
     },
 	},
 })
