5ab908bfbdce7f2ffafa3adc11ec34b6492ddedd
Askinga <musicgemca@gmail.com>
2025-11-07 20:06:33 -0500

    Update logs.js

--- js/logs.js
+++ js/logs.js
@@ -10,13 +10,13 @@ addLayer("logs", {
     }},
 	automate(){
 		if (hasMilestone('su', 2)) {
-		    if (buyableCanAfford("logs", 11)) setBuyableAmount("logs", 11, player.logs.points.max(100).log(10).sub(1).root(1).floor().add(1))
-			if (buyableCanAfford("logs", 12)) setBuyableAmount("logs", 12, player.logs.points.max(10).log(1.125).sub(1).root(1).floor().add(1))
-			if (buyableCanAfford("logs", 13)) setBuyableAmount("logs", 13, player.logs.points.max(100).log(1.2).sub(1).root(1).floor().add(1))
+		    if (canBuyBuyable("logs", 11)) setBuyableAmount("logs", 11, player.logs.points.max(100).log(10).sub(2).root(1).floor().add(1))
+			if (canBuyBuyable("logs", 12)) setBuyableAmount("logs", 12, player.logs.points.max(10).log(1.125).sub(1).root(1).floor().add(1))
+			if (canBuyBuyable("logs", 13)) setBuyableAmount("logs", 13, player.logs.points.max(100).log(1.2).sub(2).root(1).floor().add(1))
 			if (hasUpgrade('logs', 15)) {
-			    if (buyableCanAfford("logs", 21)) setBuyableAmount("logs", 21, player.logs.points.max(1000000).log(10).sub(1).root(1.1).floor().add(1))
-				if (buyableCanAfford("logs", 22)) setBuyableAmount("logs", 22, player.logs.points.max(1000000).log(1.3).sub(1).root(1).floor().add(1))
-				if (buyableCanAfford("logs", 23)) setBuyableAmount("logs", 23, player.logs.points.max(1000000).log(1.4).sub(1).root(1.2).floor().add(1))
+			    if (canBuyBuyable("logs", 21)) setBuyableAmount("logs", 21, player.logs.points.max(1000000).log(10).sub(6).root(1.1).floor().add(1))
+				if (canBuyBuyable("logs", 22)) setBuyableAmount("logs", 22, player.logs.points.max(1000000).log(1.3).sub(6).root(1).floor().add(1))
+				if (canBuyBuyable("logs", 23)) setBuyableAmount("logs", 23, player.logs.points.max(1000000).log(1.4).sub(6).root(1.2).floor().add(1))
 			}
 		}
 	},
