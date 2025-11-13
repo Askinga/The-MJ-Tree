51201868103a5e79e61753d1753ae951aa0814e7
Askinga <musicgemca@gmail.com>
2025-11-13 17:54:57 -0500

    Create tBoost.js

---
+++ js/tBoost.js
@@ -0,0 +1,31 @@
+addLayer("tb", {
+    name: "time boosters", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "TB", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#ffffff",
+    requires: new Decimal(1e13), // Can be a function that takes requirement increases into account
+    resource: "Time Boosters", // Name of prestige currency
+    baseResource: "Time", // Name of resource prestige is based on
+    baseAmount() {return player.su.timeSubtab}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.0075, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 4, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "t", description: "T: Reset for time boosters", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    effect(){ return player.tb.points.add(1).pow(2) },
+    effectDescription(){ return "which is boosting Time and SuR by x" + format(layers.tb.effect()) },
+    layerShown(){return (hasMilestone('su', 6) || player.tb.unlocked)},
+  	branches: ["su"]
+})
