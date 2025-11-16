e45ccf20a1aa295155e91cdd91eeadd6129908c9
Askinga <musicgemca@gmail.com>
2025-11-15 23:48:27 -0500

    Update money.js

--- js/money.js
+++ js/money.js
@@ -1,7 +1,7 @@
 addLayer("money", {
     name: "Money", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "$", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 2, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: false,
 		points: new Decimal(0),
@@ -25,5 +25,14 @@ addLayer("money", {
         {key: "M", description: "Shift+M: Reset for $", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return (hasUpgrade('su', 45) || player.money.unlocked)},
-	branches: ["logs"]
+	branches: ["logs"],
+	upgrades: {
+		11: {
+			title: "We have money!",
+			description: "Boost wood effect exponent based on $",
+			cost: new Decimal(1),
+			effect(){ return player.money.points.add(2).pow(0.18) },
+			effectDisplay(){ return "x"+format(upgradeEffect(this.layer, this.id)) },
+		},
+	},
 })
