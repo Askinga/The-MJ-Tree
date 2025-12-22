e580d02814732c3d833497885be337393a5bac3e
Askinga <musicgemca@gmail.com>
2025-12-22 16:02:48 -0500

    Update powers.js

--- js/powers.js
+++ js/powers.js
@@ -13,7 +13,7 @@ addLayer("pr", {
     baseAmount() {return player.points.add(1).log(10)}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
 	base: 10,
-    exponent: 1.1, // Prestige currency exponent
+    exponent: 1.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
