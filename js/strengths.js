42de3caa065b1c00e30df13a4f5ded8ad7bb30b8
Askinga <musicgemca@gmail.com>
2025-12-26 22:20:25 -0500

    Update strengths.js

--- js/strengths.js
+++ js/strengths.js
@@ -21,7 +21,7 @@ addLayer("st", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 5, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "t", description: "T: Reset for strength runes (Uni. 1)", onPress(){if (canReset(this.layer) && !(inChallenge('universes', 11))) doReset(this.layer)}},
     ],
