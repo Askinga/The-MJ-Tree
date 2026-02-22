86f7c4a727f7ff7a1d17319b1db9d20aa4e42753
Askinga <musicgemca@gmail.com>
2026-02-21 20:55:09 -0500

    Update powers.js

--- js/powers.js
+++ js/powers.js
@@ -204,5 +204,8 @@ addLayer("pr", {
 		
 		upg4 = upg4.times(diff)
 		player.pr.upg4 = player.pr.upg4.add(upg4)
+		if (hasUpgrade('en', 13)) {
+			player.pr.points = player.points.log10().div("1e348").div(new Decimal(10).pow(player.pr.points.pow(tmp.pr.exponent))).add(1)
+		},
 	},
 })
