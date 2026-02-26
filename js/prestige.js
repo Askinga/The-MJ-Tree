ff2d92c1e4e76c2046c4ee6e1c3f01a964965549
Askinga <musicgemca@gmail.com>
2026-02-25 19:59:08 -0500

    Update prestige.js

--- js/prestige.js
+++ js/prestige.js
@@ -53,7 +53,7 @@ addLayer("p", {
     color: "#00aadd",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "Rune Fragments", // Name of resource prestige is based on
     baseAmount(){return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
