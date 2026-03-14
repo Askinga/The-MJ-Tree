21e099dab23114678547bb8c17f4a92baacffd36
Askinga <musicgemca@gmail.com>
2026-03-14 17:33:25 -0400

    Update limit.js

--- js/limit.js
+++ js/limit.js
@@ -460,14 +460,14 @@ addLayer("limit", {
       goalDescription: function () {
         return (
           format(
-            new Decimal(10).pow(new Decimal(2).pow(challengeCompletions("limit", 11).add(999)))
+            new Decimal(10).pow(new Decimal(2).pow(new Decimal(challengeCompletions("limit", 11)).add(999)))
           ) + " Points"
         );
       },
       completionLimit: 10,
       canComplete: function () {
         return player.points.gte(
-          new Decimal(10).pow(new Decimal(2).pow(challengeCompletions("limit", 11).add(999)))
+          new Decimal(10).pow(new Decimal(2).pow(new Decimal(challengeCompletions("limit", 11)).add(999)))
         );
       },
       unlocked() {
