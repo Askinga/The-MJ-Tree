deb408486222764037a9aecef20bbb725ec9115b
Askinga <musicgemca@gmail.com>
2026-08-02 16:56:49 -0400

    Create truemeta.js

---
+++ js/truemeta.js
@@ -0,0 +1,35 @@
+addLayer("tm", {
+    name: "True Meta", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "TM", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#ffffff",
+    requires: new Decimal("eee100"), // Can be a function that takes requirement increases into account
+    resource: "True Meta Runes", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 7, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "T", description: "Shift+T: Reset for True Meta Runes (Uni. 1)", onPress(){if (canReset(this.layer) && !(inChallenge('universes', 11))) doReset(this.layer)}},
+    ],
+    layerShown(){return (hasUpgrade('dr', 25) || player.tm.unlocked)},
+   	branches: ["limit"],
+    effect(){
+      return new Decimal(10).pow(player.tm.points)
+    },
+    effectDescription(){
+      return "which is boosting ALL Universe 1 currencies by x"+format(layers.tm.effect())
+    },
+})
