fc4448731148264947cea316d39c4ee2d41b243c
Askinga <musicgemca@gmail.com>
2026-08-07 08:24:40 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -123,7 +123,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.tm.points.gte(new Decimal("1"))
 }
 
 
