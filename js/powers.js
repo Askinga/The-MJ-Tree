c3f7de31d8ecce5bb18fa3f6dfb6703d131f332a
Askinga <musicgemca@gmail.com>
2026-08-08 21:55:33 -0400

    Update powers.js

--- js/powers.js
+++ js/powers.js
@@ -255,6 +255,7 @@ addLayer("pr", {
 		if (hasUpgrade('pr', 34)) upg4 = upg4.times(10)
 		if (hasUpgrade('pr', 35)) upg4 = upg4.times(10)
 		upg4 = upg4.times(layers.st.effect())
+		upg4 = upg4.times(layers.tm.effect())
 		upg4 = upg4.times(tmp.limit.limitBoost)
 		if (hasUpgrade('limit', 23)) upg4 = upg4.times(upgradeEffect('limit', 23))
 		if (hasUpgrade('limit', 25)) upg4 = upg4.times(upgradeEffect('limit', 25))
