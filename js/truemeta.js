1ba983f1628e14c1378bb66ac1ec14869e946b37
Askinga <musicgemca@gmail.com>
2026-09-11 19:30:36 -0400

    Update truemeta.js

--- js/truemeta.js
+++ js/truemeta.js
@@ -7,6 +7,9 @@ addLayer("tm", {
 		points: new Decimal(0),
 		tmpoints: new Decimal(0),
 		lpow: new Decimal(0),
+		tmPow: new Decimal(0),
+		tmPowGain: new Decimal(0),
+		total: new Decimal(0),
     }},
 	onPrestige(){
 	    player.tm.tmpoints = player.tm.tmpoints.add(1)
@@ -20,11 +23,18 @@ addLayer("tm", {
     exponent: 0, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+		mult = mult.times(tmp.tm.tmPow)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
+	tmPow() {
+		return player.tm.tmPow.add(1).pow(0.08)
+	},
+	tmPowBoost(){
+		return new Decimal(1.1).pow(player.tm.total)
+	},
     row: 7, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "T", description: "Shift+T: Reset for True Meta Runes (Uni. 1)", onPress(){if (canReset(this.layer) && !(inChallenge('universes', 11))) doReset(this.layer)}},
@@ -55,6 +65,15 @@ addLayer("tm", {
 				"clickables",
 				"blank",
 				"upgrades",
+			],
+		},
+		"True Meta Power": {
+			content: [
+				["display-text", function(){ return "You have " + format(player.tm.tmPow) + " True Meta Power (" + format(player.tm.tmPowGain) + "/sec), which is boosting True Meta Runes by x" + format(tmp.tm.tmPow)}],
+				"blank",
+				"resource-display",
+				"blank",
+				["display-text", function(){ return "Every True Meta Rune you get boosts True Meta Power by x1.1! Currently: x" + format
 			],
 		},
 	},
@@ -100,6 +119,15 @@ addLayer("tm", {
 			currencyInternalName: "tmpoints",
 			currencyLayer: "tm",
 		},
+		32: {
+			title: "TM MixUp 1",
+			description: "Start with 10 Limit Resets and unlock True Meta Power.",
+			cost: new Decimal(2),
+			unlocked(){ return (hasUpgrade('tm', 21) && hasUpgrade('tm', 22)) },
+			currencyDisplayName: "True Meta Points",
+			currencyInternalName: "tmpoints",
+			currencyLayer: "tm",
+		},
 	},
 	clickables: {
     11: {
@@ -114,5 +142,14 @@ addLayer("tm", {
 		onClick(){ return player.tm.lpow = new Decimal(0) },
 		unlocked(){ return hasUpgrade('tm', 11) },
     },
+	},
+	update(diff) {
+		let gain = new Decimal(0)
+		if (hasUpgrade('tm', 32)) gain = gain.add(1)
+		gain = gain.times(tmp.tm.tmPowBoost)
+
+		player.tm.tmPowGain = gain
+		gain = gain.times(diff)
+		player.tm.tmPow = player.tm.tmPow.add(gain)
 	},
 })
