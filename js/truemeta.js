7679bb5f9b7042a59893ee4e6766cb314e2a4196
Askinga <musicgemca@gmail.com>
2026-09-18 19:27:43 -0400

    Update truemeta.js

--- js/truemeta.js
+++ js/truemeta.js
@@ -95,6 +95,7 @@ addLayer("tm", {
 		let chance = new Decimal(0.04)
 		if (hasUpgrade('tm', 101)) chance = chance.add(0.01)
 		if (hasUpgrade('tm', 102)) chance = chance.add(upgradeEffect('tm', 102).div(100))
+		if (hasUpgrade('tm', 122)) chance = chance.add(0.015)
 		return chance
 	},
     row: 7, // Row the layer is in on the tree (0 is the first row)
@@ -393,8 +394,17 @@ addLayer("tm", {
 		121: {
 			title: "TM Upg 9",
 			description: "Ts gain x10.",
-			cost: new Decimal(1250),
+			cost: new Decimal(750),
 			unlocked(){ return (hasUpgrade('tm', 113)) },
+			currencyDisplayName: "True Meta Points",
+			currencyInternalName: "tmpoints",
+			currencyLayer: "tm",
+		},
+		122: {
+			title: "TM Upg 10",
+			description: "Super True Meta Point chance +1.5%.",
+			cost: new Decimal(700),
+			unlocked(){ return (hasUpgrade('tm', 121)) },
 			currencyDisplayName: "True Meta Points",
 			currencyInternalName: "tmpoints",
 			currencyLayer: "tm",
